37e34004a41816c516e7bfe6ecce235ded382694
Élio Carlos Ferreira Segundo <esegundo@unicamp.br>
2025-04-14 17:02:50 -0300

    Translate + Open Spotfy option

--- radio2.py
+++ radio2.py
@@ -1,55 +1,60 @@
 from os import system
 from pyradios import RadioBrowser
 
-# Dicionário de Dados
+# Data Dictionary
 def biblioteca(option):
     if option == '':
         Bool = False
-        print('Buscador de Radio Onine')
-        radio = input('Entre com o nome da Radio a ser buscada:  ')
+        print('Radio Onine Search')
+        radio = input('Enter the name of Radio to be search:  ')
     else:
         Bool = True
         radio = option
     
     # rs -> radio search
     if radio == '':
-        print("Saindo")
+        print("Exiting...")
         quit()
-    rs = RadioBrowser()
-    station = rs.search(name=radio, name_exact=Bool)
+    elif radio.lower() == 'spotify':
+        station = "spotify"
+    else:
+        rs = RadioBrowser()
+        station = rs.search(name=radio, name_exact=Bool)
     return station
 
-# Cria Menu
+# Create Menu
 def menu(station):
     print()
     print('--- MENU Radio ---')
     print()
     n_station = range(len(station))
-    print("0- Sair")
+    print("0- Exit")
     for n in n_station:
         print(str(n+1)+ "- " + station[n]['name'])
-    option_num = input('Entre com o número da radio: ')
+    option_num = input('Enter the number of radio: ')
     if option_num == "0":
-        print("Saindo")
+        print("Exiting...")
         quit()
     option = station[int(option_num)-1]['name']
     return option
 
-# Função Main
+# Main Function
 def main():
     option = ''
     station = biblioteca(option)
-    option = menu(station)
-    station = biblioteca(option)
-    url = station[0]['url']
-    print(url)
-    system("cvlc " + url)
+    if station == 'spotify':
+        print("Open Spotify")
+        system("spotify")
+    else:
+        option = menu(station)
+        station = biblioteca(option)
+        url = station[0]['url']
+        print(url)
+        system("cvlc " + url)
     
 
 # Chamada do main
 if __name__ == "__main__":
     main()
-
-
 
 
